7b7a0611b805f3fdc0da7dc22dab38119d9a7a25
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-11 19:01:58 +0530

    function in python

--- 00_python_fundamentals/15-functions.py
+++ 00_python_fundamentals/15-functions.py
@@ -1,2 +1,26 @@
-#  functions: functions are basically a block of statements that performs a specific task
+#  functions: functions are basically a block of statements that performs a specific task -> reusable component of code
+    # - Function definition - writing entire logic
+    # - Function call - invoke the function - execute the function login
+    
+    # improves reusability and modularity
+    
+def printHello(): # function definition
+    print("Hello")
+    print('Hello Python')
+    
+printHello() # function call
+printHello() # function call
+printHello() # function call
 
+
+'''
+
+input(parameters) -> login -> output(return)
+
+
+'''
+
+def sum(a,b): # parameters
+    return a+b
+
+print(sum(10,11)) # arguments
