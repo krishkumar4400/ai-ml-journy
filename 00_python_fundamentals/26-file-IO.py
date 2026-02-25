6ef88314aaf0cc15cce3612f4bc968be76585def
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-25 23:00:12 +0530

    file handling in python

--- 00_python_fundamentals/26-file-IO.py
+++ 00_python_fundamentals/26-file-IO.py
@@ -1,15 +1,49 @@
-# File I/O - File input and output
+# # File I/O - File input and output
 
-# operations: Read, Write, Append
+# # operations: Read, Write, Append
 
-# File operations
-#   Open, read & close
+# # File operations
+# #   Open, read & close
 
-f = open("./26-test.txt", "r") # return file object
+# f = open("./26-test.txt", "r")  # return file object
 
-data = f.read()
+# # data = f.read()
 
-print(data)
-print(type(data))  # <class 'str'>
+# # data = f.readline()
+
+# # data = f.readlines()
+
+# data = f.readline()
+# print(data)
+# data = f.readline()
+# print(data)
+# data = f.readline()
+# print(data)
+# data = f.readline()
+# print(data)
+# data = f.readline()
+# print(data)
+# data = f.readline()
+# print(data)
+
+
+# print(data)
+# print(type(data))  # <class 'str'>
+
+
+# f = open("26-test.txt", 'w')
+# f.write("This is new text\n the complete data")
+
+# f.close()
+
+
+# f = open('26-test.txt', 'a')
+
+# f.write("\n\nNew text being appended to the file")
+# f.close()
+
+f = open('26-sample.txt', 'x')
+
+f.write("New file created")
 
 f.close()
