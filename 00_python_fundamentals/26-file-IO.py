374bcc017855559d2657fffb893edeb51304bebd
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-25 20:32:04 +0530

    file i/o

--- 00_python_fundamentals/26-file-IO.py
+++ 00_python_fundamentals/26-file-IO.py
@@ -1,2 +1,15 @@
 # File I/O - File input and output
 
+# operations: Read, Write, Append
+
+# File operations
+#   Open, read & close
+
+f = open("./26-test.txt", "r") # return file object
+
+data = f.read()
+
+print(data)
+print(type(data))  # <class 'str'>
+
+f.close()
