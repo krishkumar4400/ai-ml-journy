581550122d192bd5bc8f55b86241226bd17113df
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-27 00:20:44 +0530

    file io - file deletion

--- 00_python_fundamentals/26-file-IO.py
+++ 00_python_fundamentals/26-file-IO.py
@@ -61,19 +61,47 @@
 
 
 # With keyword
-with open("26-sample.txt", 'r') as f:
-    # print(f.read())
-    # print(f.read())
-    # print(f.readline())
-    data = f.read()
-    print(type(data))
-    print(len(data))
-    print(data)
-    
-with open("26-sample.txt") as f:
+# with open("26-sample.txt", 'r') as f:
+#     # print(f.read())
+#     # print(f.read())
+#     # print(f.readline())
+#     data = f.read()
+#     print(type(data))
+#     print(len(data))
+#     print(data)
+
+# with open("26-sample.txt") as f:
+#     data = f.readlines()
+#     print(data)
+#     print(type(data))
+#     print(len(data))
+
+
+# # delete files
+# import os
+# os.remove('./26-sample.txt')
+
+
+# # Practice Problem
+# with open('26-sample.txt', 'r') as f:
+#     data = f.read()
+#     lst = data.split(' ')
+#     idx = 0
+#     print(lst)
+#     for val in lst:
+#         if val == 'Python':
+#             print(val)
+#             print(idx)
+#         idx+=1
+
+# Practice Problem
+with open("26-sample.txt", "r") as f:
     data = f.readlines()
-    print(data)
-    print(type(data))
-    print(len(data))
-    
-# delete files
+    line=0
+    for val in data:
+        lst = val.split(" ")
+        for word in lst:
+            if(word == "Python"):
+                print(word)
+                print(line)
+        line+=1
